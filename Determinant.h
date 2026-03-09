9cc456be6f4e33c4f32f333e3612613727c85f89
quark_cephi <suppritdas@gmail.com>
2026-03-09 23:43:44 +0530

    Header file error fixed

--- Determinant.h
+++ Determinant.h
@@ -1,45 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "determinant.h"
+#ifndef DET_H
+#define DET_H
 
-double determinant(double **a, int n)
-{
-    double det = 0;
-    double **temp;
-    int sign = 1;
+double determinant(double **a, int n);
 
-    if (n == 1)
-        return a[0][0];
-
-    temp = (double**)malloc((n-1)*sizeof(double*));
-    for(int i=0;i<n-1;i++)
-        temp[i]=(double*)malloc((n-1)*sizeof(double));
-
-    for(int f=0; f<n; f++)
-    {
-        int subi=0;
-
-        for(int i=1;i<n;i++)
-        {
-            int subj=0;
-
-            for(int j=0;j<n;j++)
-            {
-                if(j==f) continue;
-
-                temp[subi][subj]=a[i][j];
-                subj++;
-            }
-            subi++;
-        }
-
-        det += sign * a[0][f] * determinant(temp,n-1);
-        sign = -sign;
-    }
-
-    for(int i=0;i<n-1;i++)
-        free(temp[i]);
-    free(temp);
-
-    return det;
-}
+#endif
